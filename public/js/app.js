d7c25a74a74350b7c0ccd2715f616db360d83cb4
kushagramandaliya <kush98mandaliya@gmail.com>
2021-06-26 17:07:26 +0530

    app.js changed

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Public side javascript')
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageone = document.querySelector('#message-1')
